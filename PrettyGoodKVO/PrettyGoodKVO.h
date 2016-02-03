4080343b4fb9843dac6722658b32880a55fc796e
Jed Lewison <jed@magicappfactory.com>
2016-02-02 22:54:45 -0800

    Initial Commit

---
+++ PrettyGoodKVO/PrettyGoodKVO.h
@@ -0,0 +1,19 @@
+//
+//  PrettyGoodKVO.h
+//  PrettyGoodKVO
+//
+//  Created by Jed Lewison on 2/2/16.
+//  Copyright © 2016 Magic App Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrettyGoodKVO.
+FOUNDATION_EXPORT double PrettyGoodKVOVersionNumber;
+
+//! Project version string for PrettyGoodKVO.
+FOUNDATION_EXPORT const unsigned char PrettyGoodKVOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrettyGoodKVO/PublicHeader.h>
+
+
